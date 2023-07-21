fbd556869304364bde425a7bc2390a663bfcf14a
Torbenk88@hotmail.com <tk@evobis.dk>
2023-07-21 13:49:17 +0200

    Switched to a onepage layout instead, and changed meta info

--- src/Forside.tsx
+++ src/Forside.tsx
@@ -27,23 +27,23 @@ export const Forside: React.FunctionComponent<IAppProps> = (props: React.PropsWi
   // const backgroundVideo = useCountries().background;
   // const travelBoxes = useCountries().travelBoxes;
   // const [backgroundVideo, loading] = useBackgroundVideo("Travel")
-  const onPageChange = (side: Sider) => {
-    localStorage.setItem('activePage', side.toString())
-    setActivePage(side)
-  }
+  // const onPageChange = (side: Sider) => {
+  //   localStorage.setItem('activePage', side.toString())
+  //   setActivePage(side)
+  // }
 
-  const onProjectChange = (project: string) => {
-    localStorage.setItem('selectedProject', project)
+  // const onProjectChange = (project: string) => {
+  //   localStorage.setItem('selectedProject', project)
 
-    setSelectedProject(project)
-  }
+  //   setSelectedProject(project)
+  // }
 
-  const onReset = () => {
-    localStorage.removeItem('selectedProject')
-    localStorage.removeItem('activePage')
-    setSelectedProject("")
-    setActivePage(Sider.Portfolio)
-  }
+  // const onReset = () => {
+  //   localStorage.removeItem('selectedProject')
+  //   localStorage.removeItem('activePage')
+  //   setSelectedProject("")
+  //   setActivePage(Sider.Portfolio)
+  // }
 
   // const clearLocalStorage = () => {
   //   localStorage.removeItem('selectedProject')
@@ -56,29 +56,79 @@ export const Forside: React.FunctionComponent<IAppProps> = (props: React.PropsWi
   //     window.removeEventListener("beforeunload", clearLocalStorage);
   //   };
   // }, []);
+  React.useEffect(() => {
+    const handleScroll = (event: any) => {
 
+      const scrollTop = event.target.scrollTop;
+      const navItems = Array.from(document.querySelectorAll('.Navigation li'));
+      navItems.map((navItem: any) => {
+        const cID = navItem.querySelector('a').getAttribute('href');
+        const container = document.querySelector(cID).offsetTop;
+        if (scrollTop >= container) {
+          const activeItems = Array.from(document.querySelectorAll('.Navigation li.active'));
+          activeItems.map(item => item.classList.remove('active'))
+          navItem.classList.add('active')
+        }
+      })
+    };
+
+    document.querySelector('.udviklerFar')?.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
     < >
-      {selectedProject == "" &&
-        <div className='udviklerFar'>
-          <Navigation onPageChange={onPageChange} currentPage={activePage} />
+      {/* {selectedProject == "" && */}
+      <div className='udviklerFar' onScroll={e => {
+      }}>
+        <ul className='Navigation'>
+          <li className='active'>
+            <a href='#Forside'>FORSIDE</a>
+          </li>
+          <li>
+            <a href='#CV'>CV</a>
+          </li>
+          <li>
+            <a href='#Portfolio'>Portfolio</a>
+          </li>
+          <li>
+            <a href='#OmMig'>OmMig</a>
+          </li>
+        </ul>
+        {/* <Navigation onPageChange={onPageChange} currentPage={activePage} />
           {activePage == Sider.Portfolio && <Portfolio onProjectSelect={onProjectChange} selectedProject={selectedProject} />}
           {activePage == Sider.CV && <CV></CV>}
           {activePage == Sider.Ommig && <OmMig></OmMig>}
           {activePage == Sider.Kontakt && <Kontakt></Kontakt>}
-          {activePage == Sider.Forside &&
-            <div className='Main'>
-              {/* <Header /> */}
-              <div className='animate__animated animate__fadeInUp animate__delay-1s animate__slow'>
+          {activePage == Sider.Forside && */}
+        {/* <div style={{ height: "100%" }}> */}
+        {/* <div style={{ height: "100%" }}> */}
+        {/* </div> */}
+        {/* <Header /> */}
 
-                <h1>Velkommen til udviklerfar!</h1>
-                <p style={{ justifyContent: "center", display: "flex", fontSize: 24, marginTop: 0 }}>Denne hjemmeside kan give dig et indblik i hvem jeg er som person og udvikler.</p>
-              </div>
-            </div>
-          }
+        {/* </div> */}
+        <div id="Forside" className='Main' >
+
+          <div className='animate__animated animate__fadeInUp animate__delay-1s animate__slow'>
+            <h1>Velkommen til udviklerfar!</h1>
+            <p style={{ justifyContent: "center", display: "flex", fontSize: 24, marginTop: 0 }}>Denne hjemmeside kan give dig et indblik i hvem jeg er som person og udvikler.</p>
+          </div>
         </div>
-      }
-      {selectedProject == "FarTravels" && <FarTravels onReturn={onReset} />}
+        <div id="CV" className='center col' >
+          <CV></CV>
+        </div>
+        <div id="Portfolio" className='center col' >
+          <Portfolio onProjectSelect={() => { }} selectedProject={selectedProject} />
+        </div>
+        <div id="OmMig" className='center col' >
+          <OmMig></OmMig>
+        </div>
+
+      </div>
+      {/* } */}
+      {/* {selectedProject == "FarTravels" && <FarTravels onReturn={onReset} />} */}
     </>
   );
 };
--- src/Ommig/Ommig.tsx
+++ src/Ommig/Ommig.tsx
@@ -8,16 +8,16 @@ export const OmMig: React.FunctionComponent<IOmMigProps> = (props: React.PropsWi
   const sideBillede = require('./../assets/Theo_2.jpg');
 
   return (
-    <div className='Wrapper row' style={{ justifyContent: "center" }}>
+    <div className='Wrapper' style={{ justifyContent: "center" }}>
       <div className="ommig">
         <div className='ommigBillede' style={{ backgroundImage: `url(${sideBillede})`}} >
           {/* <img width={"30%"} src={sideBillede} className='animate__animated animate__fadeIn'></img> */}
         </div>
         <div className='ommigtekst' >
           <h3 className='sektionNavn' style={{ marginTop: 0, marginBottom: 0, textTransform: "uppercase" }}>Hvem er udviklerfar?</h3>
-          <p>Mit navn er Torben, jeg er 34 år og er bosat i Ikast sammen med min kone og min søn på lidt over 1 år. </p>
+          <p>Mit navn er Torben, jeg er 34 år og er bosat i Ikast sammen med min kone og min søn. </p>
           <p>Jeg bruger meget af min fritid på gaming og film og nyder også en cykeltur i ny og næ, samt spille lidt guitar når tiden er til det. </p>
-          <p>Personligt er jeg er meget nede på jorden, og social smilende person, som forsøger at sprede smil hvor jeg kan</p>
+          <p>Personligt er jeg er meget nede på jorden, og social smilende person, som forsøger at sprede smil hvor jeg kan.</p>
           <p></p>
           <p>Jeg startede udviklerfar, med henblik på primært at fremvise mine grafiske evner, da efter at have været Sharepoint udvikler i 5+ år, har det været svært at vise sin kreative side frem, så det håber jeg at denne hjemmeside kan hjælpe med.</p>
 
